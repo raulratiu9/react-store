2ca497b3ca1aeee52ffe8b0d6cac0e04104da1e1
raulratiu9 <raulratiu2@gmail.com>
2021-10-22 17:19:31 -0700

    added React routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from "./logo.svg";
-import HomePage from "./pages/homepage/homepage.component";
-import "./App.css";
+import logo from './logo.svg';
+import HomePage from './pages/homepage/homepage.component';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
