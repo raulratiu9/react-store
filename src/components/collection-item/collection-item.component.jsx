afbc99c3a95d50af44928d451a6b0a851408c34f
raulratiu9 <raulratiu2@gmail.com>
2021-11-02 23:47:36 -0700

    bugs fixes and more redux connections and a category page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addItem } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
       <div
@@ -25,6 +27,7 @@ const CollectionItem = ({ item, addItem }) => {
     </div>
   );
 };
+
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item)),
 });
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router';
+import { selectCollections } from '../../redux/shop/shop.selectors';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection/collection.component';
 
-import SHOP_DATA from './shop.data.js';
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
